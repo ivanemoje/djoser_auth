866676a23a9fa8509994d0c2c484bfd3e056d4e1
Ivan Emoje <Ivan.Emoje@wfp.org>
2020-02-11 10:22:22 +0300

    Djoser with mysql

--- ver_django/settings.py
+++ ver_django/settings.py
@@ -89,7 +89,7 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -112,7 +112,7 @@ DATABASES = {
         },
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
